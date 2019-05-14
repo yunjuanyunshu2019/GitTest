9d44a42cc73e8674aa63b101a789593c02fac670
YTKJ <>
2019-05-14 15:32:02 +0800

    no message

--- GitTest/GitTest/ViewController.m
+++ GitTest/GitTest/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     self.view.backgroundColor = UIColor.whiteColor;
     NSLog(@"11111");
+    NSLog(@"uuuu");
 
     // Do any additional setup after loading the view, typically from a nib.
 }
